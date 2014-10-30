2ec92e84905beb736b02cd9ed5b85ee76a367085
Andrew J. Musselman <ki6uoc@gmail.com>
2014-10-30 00:47:36 -0700

    First model, tried and failed to register to admin panel

--- rvgs/gamequest/admin.py
+++ rvgs/gamequest/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from gamequest.models import System
 
 # Register your models here.
+admin.site.register(System)
--- rvgs/gamequest/models.py
+++ rvgs/gamequest/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class System(models.Model):
+    """
+    An object to represent a type of game platform. Keeps track of systems that
+    emulate other systems. 
+    """
+    systemName = models.CharField(max_length=100)
+    systemDescription = models.CharField(max_length=512)
+    emulated = models.BooleanField(default=False)
+    emulating = models.ForeignKey("System")
+
+
